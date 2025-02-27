ff3e19db82b5e7f814bf09d5b0bec8ec5d42beba
Erzhan <erzhanmuratov99@gmail.com>
2025-02-27 16:48:02 +0600

    add conventions

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,12 +1,18 @@
 from sqlalchemy.orm import (
 	DeclarativeBase, Mapped, mapped_column, declared_attr
 )
+from sqlalchemy import MetaData
 
+from core.config import settings
 from utils.case_converter import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
 	__abstract__ = True
+
+	metadata = MetaData(
+		naming_convention=settings.db.naming_convention,
+	)
 
 	@declared_attr.directive
 	def __tablename__(cls) -> str:
